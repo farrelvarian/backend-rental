8fb28204f212dc3fc7a59e6396cea63a91337d5e
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-09-14 10:22:25 +0700

    initial heroku

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cookieParser = require("cookie-parser");
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-const optionCors = { credentials: true, origin:`${process.env.FRONT_URL}` };
+const optionCors = { credentials: true, origin:true };
 app.use(cors(optionCors));
 // app.use((_, res, next) => {
 //   res.header("Access-Control-Allow-Origin", true);
