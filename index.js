6347d7792ee466a4af583ee946196d4e5765c91f
Farrel Varian Eka Putra <42968960+farrelvarian@users.noreply.github.com>
2021-09-14 10:15:51 +0700

    initial heroku

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ const cookieParser = require("cookie-parser");
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-// const optionCors = { credentials: true, origin:`${process.env.FRONT_URL}` };
-// app.use(cors(optionCors));
-app.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", true);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ); // If needed
-  res.header(
-    "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  ); // If needed
-  res.header("Access-Control-Allow-Credentials", true); // If needed
-  next();
-});
+const optionCors = { credentials: true, origin:`${process.env.FRONT_URL}` };
+app.use(cors(optionCors));
+// app.use((_, res, next) => {
+//   res.header("Access-Control-Allow-Origin", true);
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   ); // If needed
+//   res.header(
+//     "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   ); // If needed
+//   res.header("Access-Control-Allow-Credentials", true); // If needed
+//   next();
+// });
 app.use(cookieParser());
 
 app.use("/vehicles", vehicleRouter);
